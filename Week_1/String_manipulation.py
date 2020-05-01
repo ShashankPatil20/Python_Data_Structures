ec52331346fe83615f172c9afe59d0051853d1a0
ShashankPatil20 <patil.shashankssp@gmail.com>
2020-05-01 19:20:03 +0530

    Solutions to Course

---
+++ Week_1/String_manipulation.py
@@ -0,0 +1,7 @@
+#Stripping Whitespace
+
+greet= '     Whitespace   '
+print(greet.lstrip())
+
+line = 'have a nice day'
+print(line.startswith('h'))
---
+++ Week_1/Strings.py
@@ -0,0 +1,10 @@
+fruits = 'banana'
+letter = fruits[1]
+print(letter)
+
+x = 4
+y = fruits[x-1]
+print(y)
+
+print(len(fruits))
+print(fruits[:])
